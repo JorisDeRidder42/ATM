58d4f990640201b2f89cb37fe06ebb270b33d1af
JorisDeRidderY <Joris.deridder@yucopia.com>
2022-07-11 15:18:40 +0200

    eerste commit^

---
+++ ATM_JorisDeRidder_WPF/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using ATM_JorisDeRidder_WPF.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ATM_JorisDeRidder_WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            HomeViewModel HviewModel = new HomeViewModel();
+            View.HomeView hview = new View.HomeView();
+            hview.DataContext = HviewModel;
+            hview.Show();
+        }
+    }
+}
---
+++ ATM_JorisDeRidder_WPF/ViewModel/HomeViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM_JorisDeRidder_WPF.ViewModel
+{
+    public class HomeViewModel : BasisViewModel
+    {
+        public override string this[string columnName] => throw new NotImplementedException();
+
+        public override bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public override void Execute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

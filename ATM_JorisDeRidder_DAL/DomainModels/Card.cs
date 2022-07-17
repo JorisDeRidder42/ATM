6f1d8838653c9690dca7c16cb1ab4eb7905ada00
JorisDeRidder42 <Jorisderidder@outlook.be>
2022-07-17 07:42:37 +0200

    create classes and added passwordboxes

---
+++ ATM_JorisDeRidder_DAL/DomainModels/Card.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM_JorisDeRidder_DAL.DomainModels
+{
+    public class Card
+    {
+        [Key]
+        public int CardID { get; set; }
+
+        [Required]
+        public string CardNumber { get; set; }
+
+        [Required]
+        public string CardPassword { get; set; }
+
+        public int FailedLogin { get; set; }
+    }
+}
---
+++ ATM_JorisDeRidder_DAL/DomainModels/Client.cs
@@ -0,0 +1,38 @@
+﻿using JullieZijnGeslaagd_DAL.BasisModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM_JorisDeRidder_DAL.DomainModels
+{
+    public class Client : Basisklasse
+    {
+        [Key]
+        public int ClientID { get; set; }
+
+        [Required]
+        public string ClientName { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        public string Street { get; set; }
+
+        [Required]
+        public string HouseNumber { get; set; }
+
+        [Required]
+        public string ZipCode { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        public bool IsIngelogd { get; set; }
+        public DateTime BirthDate { get; set; }
+        public DateTime ContactAdded { get; set; }
+    }
+}

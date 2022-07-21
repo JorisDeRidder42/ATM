8281aa12214df0e391d9dca65d4c29b1964b301d
JorisDeRidder42 <Jorisderidder@outlook.be>
2022-07-21 10:50:21 +0200

    added fast cash view and model

---
+++ ATM_JorisDeRidder_WPF/ViewModel/CashViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM_JorisDeRidder_WPF.ViewModel
+{
+    public class CashViewModel : BasisViewModel
+    {
+        public override string this[string columnName] => throw new NotImplementedException();
+
+        public override bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public override void Execute(object parameter)
+        {
+            switch (parameter.ToString())
+            {
+                case "10": Withdraw10(); break;
+                case "20": Withdraw20(); break;
+                case "50": Withdraw50(); break;
+                case "100": Withdraw100(); break;
+                case "200": Withdraw200(); break;
+                case "500": Withdraw500(); break;
+            }
+        }
+
+        private void Withdraw500()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Withdraw200()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Withdraw100()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Withdraw50()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Withdraw20()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Withdraw10()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
